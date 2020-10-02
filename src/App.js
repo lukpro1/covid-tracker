0dad916b08a0f656d81a256cfdff9ab4f322d7b1
Łukasz Prokopiuk <lprokopiuk92@gmail.com>
2020-10-02 20:25:35 +0000

    test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import React from 'react';
-import './App.scss';
+import { MenuItem, FormControl, Select } from '@material-ui/core';
+import './styles/App.scss';
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <h1>COVID-19 tracker</h1>
+      <FormControl className="app__dropdown">
+        <Select variant="outlined" value="abc">
+          <MenuItem value="worldwide" />
+          <MenuItem value="worldwide" />
+          <MenuItem value="worldwide" />
+        </Select>
+      </FormControl>
+      {/* HEADER */}
+      {/* title + select dropdown field */}
+
+      {/* info box */}
+      {/* info box */}
+      {/* info box */}
+
+      {/* map */}
+
+      {/* table */}
+      {/* graph */}
+    </div>
+  );
 }
 
 export default App;
