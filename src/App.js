03511fa34c1972ede9b8def2baef272c8c23c57f
Łukasz Prokopiuk <lprokopiuk92@gmail.com>
2020-10-09 09:56:23 +0000

    get countries data from  API

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { MenuItem, FormControl, Select } from '@material-ui/core';
 import './styles/App.scss';
 
 function App() {
+  // STATE = HOW TO USE VARIABLE IN REACT <<<
+  const [countries, setCountries] = useState([]);
+
+  // USE EFFECT runs piece of code based on a given condition
+  useEffect(() => {
+    // the code inside here will run once
+    // when the component loads and not again
+    const getCountriesData = async () => {
+      await fetch('https://disease.sh/v3/covid-19/countries').then((response) =>
+        response.json(),
+      );
+    };
+  }, [countries]);
+  //   API Requests
+
   return (
     <div className="app">
       <h1>COVID-19 tracker</h1>
       <FormControl className="app__dropdown">
         <Select variant="outlined" value="abc">
-          <MenuItem value="worldwide" />
-          <MenuItem value="worldwide" />
-          <MenuItem value="worldwide" />
+          {/* Loop through countries and show the dropdown list
+          of these countries */}
+
+          {/* <MenuItem value="worldwide"> Option 1</MenuItem>
+          <MenuItem value="worldwide"> Option 2</MenuItem>
+          <MenuItem value="worldwide"> Option 3</MenuItem>
+          <MenuItem value="worldwide"> Option 4</MenuItem> */}
         </Select>
       </FormControl>
       {/* HEADER */}
